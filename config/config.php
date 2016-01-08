9c8544706e27b80e27c46e675059f54d63ed2aa7
Patrick Samson <patricksamson236@gmail.com>
2016-01-08 16:38:26 -0500

    Update docs

--- config/config.php
+++ config/config.php
@@ -18,7 +18,9 @@ return array(
      *
      * Sometimes you want to set the URL parameter key to be used by LocaleSwitcher
      * to use to detect locale switching requests.
-     * By default, it is : http://my-app.com/some/page/?locale=en
+     * 
+     * By default, it is "locale", so the URL will be :
+     *     http://my-app.com/some/page/?locale=en
      *
      */
     'URL_param_key' => 'locale',
@@ -29,6 +31,7 @@ return array(
      *--------------------------------------------------------------------------
      *
      * Specify the locales you want to enable in your app.
+     * Set to null to allow ANY locale
      *
      */
     'enabled_locales' => [
